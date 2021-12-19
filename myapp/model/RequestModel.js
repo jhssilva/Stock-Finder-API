59438b74d9d1633ac567812890069d1836917255
hugosilvaaUC <jhssilva96@gmail.com>
2021-12-19 17:57:17 +0000

    Code refractor Request Model
    Changed from class to a const.

--- myapp/model/RequestModel.js
+++ myapp/model/RequestModel.js
@@ -1,45 +1,38 @@
 module.exports = class RequestModel {
   constructor() {
-    this.financialModelingPrep = new FinancialModelingPrep();
-    this.typeOfRequest = new TypeOfRequest();
+    this.financialModelingPrep = financialModelingPrep;
+    this.typeOfRequest = typeOfRequest;
   }
 };
 
-class FinancialModelingPrep {
-  constructor() {
-    this.apiKey = "219bf3529861f3dc5899ec824c801709";
-    this.hostname = "financialmodelingprep.com";
-    this.port = 443;
-    this.maxCalls = 250;
-    this.version = "v3";
+const endPoints = {
+  financialStatementSymbolsList: "financial-statement-symbol-lists",
+  incomeStatement: "income-statement",
+  incomeStatementAsReported: "income-statement-as-reported",
+  balanceSheetStatement: "balance-sheet-statement",
+  balanceSheetStatementAsReported: "balance-sheet-statement-as-reported",
+  cashFlowStatement: "cash-flow-statement",
+  cashFlowStatementAsReported: "cash-flow-statement-as-reported",
+  financialStatementAsReported: "financial-statement-full-as-reported",
+  annualReportOnForm10K: "financial-reports-json",
+  companyProfile: "profile",
+  mostLosers: "losers",
+  mostActives: "actives",
+  tresuaryRates: "treasury",
+};
 
-    this.endPoints = new (class endPoints {
-      constructor() {
-        this.financialStatementSymbolsList = "financial-statement-symbol-lists";
-        this.incomeStatement = "income-statement";
-        this.incomeStatementAsReported = "income-statement-as-reported";
-        this.balanceSheetStatement = "balance-sheet-statement";
-        this.balanceSheetStatementAsReported =
-          "balance-sheet-statement-as-reported";
-        this.cashFlowStatement = "cash-flow-statement";
-        this.cashFlowStatementAsReported = "cash-flow-statement-as-reported";
-        this.financialStatementAsReported =
-          "financial-statement-full-as-reported";
-        this.annualReportOnForm10K = "financial-reports-json";
-        this.companyProfile = "profile";
-        this.mostLosers = "losers";
-        this.mostActives = "actives";
-        this.tresuaryRates = "treasury";
-      }
-    })();
-  }
-}
+const financialModelingPrep = {
+  apiKey: "219bf3529861f3dc5899ec824c801709",
+  hostname: "financialmodelingprep.com",
+  port: 443,
+  maxCalls: 250,
+  version: "v3",
+  endPoints: endPoints,
+};
 
-class TypeOfRequest {
-  constructor() {
-    this.get = "GET";
-    this.post = "POST";
-    this.put = "PUT";
-    this.delete = "DELETE";
-  }
-}
+const typeOfRequest = {
+  get: "GET",
+  post: "POST",
+  put: "PUT",
+  delete: "DELETE",
+};
