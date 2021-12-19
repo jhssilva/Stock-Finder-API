5b4181d1398070717c25047b017f1b44112961e5
hugosilvaaUC <jhssilva96@gmail.com>
2021-12-19 11:05:34 +0000

    Updated endpoints
    Added more endpoints to facilitate the implementation of the requests.

--- myapp/model/RequestModel.js
+++ myapp/model/RequestModel.js
@@ -12,11 +12,24 @@ class FinancialModelingPrep {
     this.port = 443;
     this.maxCalls = 250;
     this.version = "v3";
-    this.financialStatementSymbolsList = "financial-statement-symbol-lists";
+
     this.endPoints = new (class endPoints {
       constructor() {
         this.financialStatementSymbolsList = "financial-statement-symbol-lists";
         this.incomeStatement = "income-statement";
+        this.incomeStatementAsReported = "income-statement-as-reported";
+        this.balanceSheetStatement = "balance-sheet-statement";
+        this.balanceSheetStatementAsReported =
+          "balance-sheet-statement-as-reported";
+        this.cashFlowStatement = "cash-flow-statement";
+        this.cashFlowStatementAsReported = "cash-flow-statement-as-reported";
+        this.financialStatementAsReported =
+          "financial-statement-full-as-reported";
+        this.annualReportOnForm10k = "financial-reports-json";
+        this.companyProfile = "profile";
+        this.mostLosers = "losers";
+        this.mostActives = "actives";
+        this.tresuaryRates = "treasury";
       }
     })();
   }
